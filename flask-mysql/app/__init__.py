5aa33594a0b876eee975b01d70ea5fd25c2916ef
Minh-Quan Nguyen <Minh-Quan_Nguyen@neimanmarcus.com>
2017-11-13 10:10:14 -0600

    Initial Commit

---
+++ flask-mysql/app/__init__.py
@@ -0,0 +1,38 @@
+# ----------------------------------------------------------------------------------------------- #
+# This is a simple Flask Web Application that is deployed to your localhost using docker. The app #
+# will show you the contents of your MySQL Database built with a Docker Container.                #
+#                                                                                                 #
+# The purpose of this app is to show how you are able to connect to a database using Flask and    #
+# display its contents.                                                                           #
+#                                                                                                 #
+# Code Written: Minh-Quan H. Nguyen                                                               #
+# ----------------------------------------------------------------------------------------------- #
+
+
+# ----------------------------------- Import Libraries / File ----------------------------------- #
+from flask import Flask
+
+
+# --------------------------------------- Initialize App. --------------------------------------- #
+app = Flask(__name__)
+
+
+# --------------------------------------- Import Settings --------------------------------------- #
+'''
+Use the following code to import your settings that is written in Python.
+
+app.config.from_object('<your python setting file here>')
+'''
+
+
+# ------------------------------------- Initialize Database ------------------------------------- #
+'''
+Simply initialize a MySQL Database with Flask by using the code below. This will utilize the
+SQLAlchemy Library from Flask.
+
+db = SQLAlchemy(app) 
+'''
+
+
+# -------------------------------- Import Application Controller -------------------------------- #
+# from helloworld_web_app import helloworld_controller
